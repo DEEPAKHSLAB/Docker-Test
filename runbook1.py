28979ea070cbeb87db7fe2d430ce3575d598e440
DEEPAKHSLAB <69907854+DEEPAKHSLAB@users.noreply.github.com>
2024-01-29 09:38:54 +0530

    Create runbook1.py

---
+++ runbook1.py
@@ -0,0 +1,12 @@
+rom nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_command
+from rich import print as rp
+from nornir_utils.plugins.functions import print_result as pr
+
+nr = InitNornir(config_file="config.yaml")
+
+def uptime(task):
+     task.run(task=netmiko_send_command, command_string="show system uptime")
+
+result = nr.run(task=uptime)
+pr(result)
